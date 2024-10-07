87f50d679eb8f869a52a7bda6647f019aabc5bcf
RaphaelTW <raphael.tw22@gmail.com>
2024-10-07 20:52:20 -0300

    frist commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\WelcomeController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', WelcomeController::class);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/easter-eggs', fn () => 'Oi eu sou o Rapha');
