9a1b1cc2218902c3ee896c73ef5c2c90df57ab6c
RaphaelTW <raphael.tw22@gmail.com>
2024-10-08 14:49:10 -0300

    criacao dos models user e project

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,11 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
- */
 class ProjectFactory extends Factory
 {
     /**
@@ -16,8 +14,22 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+
+        // dd(
+        //     htmlspecialchars(fake()->randomHtml())
+        // );
+        
+        // dd(fake()->words(5),);
+
+
         return [
-            //
+            'title' => collect(fake()->words(5))->join(''),
+            'description' => fake()->randomHtml(),
+            'ends_at' => fake()->dateTimeBetween('now', '+3 days'),
+            // 'status' => fake()->randomElement('open', 'closed'),
+            'status' => fake()->randomElement(['open', 'closed']),
+            'tech_stack' => fake()->randomElements(['react', 'php', 'laravel', 'vue', 'tailwind', 'javascript', 'nextjs', 'python'], random_int(1, 5)),
+            'created_by' => User::factory(),
         ];
     }
 }
--- database/migrations/2024_10_08_153501_create_projects_table.php
+++ database/migrations/2024_10_08_153501_create_projects_table.php
@@ -3,23 +3,35 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
+    // Table projects {
+    //     id int [primary key, increment]
+    //     title string
+    //     description text
+    //     created_art datetime
+    //     ends_art datetime
+    //     tech_stack json
+    //     created_by fk [ref: < users.id]
+    //   }
+
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('ends_at');
+            $table->string('status')->default('open');
+            $table->json('tech_stack');
+            $table->foreignIdFor(User::class, 'created_by')->constrained('users');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('projects');
